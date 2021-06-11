775962fcdcc9ca64c958a7340eff34a51135f9e1
tandrus-jaroop <tandrus@jaroop.com>
2021-06-11 08:01:49 -0500

    added basic html structure without styling

--- src/todo-list/index.tsx
+++ src/todo-list/index.tsx
@@ -1,12 +1,44 @@
-import React, {FC} from 'react';
-import './index.css';
+import React, { FC } from "react";
+import "./index.css";
 /**
  * Primary UI component for user interaction
  */
 export const Todo = () => {
-  return (
-    <div>
-      Hello world!
-    </div>
-  );
+    return (
+        <div>
+            <h2>todo list</h2>
+            <div>
+                <h3>add item</h3>
+                <input />
+                <button>add</button>
+            </div>
+            <div>
+                <h3>todo</h3>
+                <ul>
+                    <li>
+                        <input type="checkbox" />
+                        <input type="text" value="Pay Bills" />
+                        <button>Edit</button>
+                        <button>Delete</button>
+                    </li>
+                    <li>
+                        <input type="checkbox" />
+                        <p>Go Shoping</p>
+                        <button>Edit</button>
+                        <button>Delete</button>
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    <li>
+                        <input type="checkbox" checked />
+                        <p>Go Shoping</p>
+                        <button>Edit</button>
+                        <button>Delete</button>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
 };
